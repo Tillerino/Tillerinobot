d5b8a5d747a82f642f2af63dd02f5ae6c1a6bd94
Tillerino <tillmann.gaida@gmail.com>
2015-02-07 14:44:57 +0100

    New version message.

--- tillerinobot/src/main/java/tillerino/tillerinobot/IRCBot.java
+++ tillerinobot/src/main/java/tillerino/tillerinobot/IRCBot.java
@@ -382,10 +382,11 @@ public class IRCBot extends CoreHooks implements TidyObject {
 		pinger.handleUnknownEvent(event);
 	}
 	
-	static final int currentVersion = 9;
-	static final String versionMessage = "Quick announcement: [http://ppaddict.tillerino.org ppaddict] has been updated with fresh data, mods, and you can now get recommendations right on the website."
-			+ " And did you know that electrolytes [https://github.com/Tillerino/Tillerinobot/wiki/Settings extended my language module]?"
-			+ " Oh and there are [https://github.com/Tillerino/Tillerinobot/wiki/Recommendations new options for recommendations].";
+	static final int currentVersion = 10;
+	static final String versionMessage = "Quick update: I now speak [https://github.com/Tillerino/Tillerinobot/blob/master/tillerinobot/src/main/java/tillerino/tillerinobot/UserDataManager.java#L62 a bunch of new languages] thanks to the contributions of your fellow players."
+			+ " If you'd like to contribute a translation as well, please [https://github.com/Tillerino/Tillerinobot/wiki/Contact get in touch]!"
+			+ " Oh and you can get hidden mods for gamma recommendations by adding \" hd\" to whatever you're requesting."
+			+ " If you haven't already done so, make sure to check out [http://ppaddict.tillerino.org/ ppaddict].";
 	
 	long lastListTime = System.currentTimeMillis();
 	
