c3c354e66fb5c4bce53a6f8f87aa4a0d76d5a9f3
ApeConfirmed <mailservx@gmail.com>
2015-11-25 11:34:26 +0100

    Removed Hungarian chars to fix the errors

--- tillerinobot/src/main/java/tillerino/tillerinobot/lang/Hungarian.java
+++ tillerinobot/src/main/java/tillerino/tillerinobot/lang/Hungarian.java
@@ -24,46 +24,46 @@ public class Default implements Language {
 
 	@Override
 	public String unknownBeatmap() {
-		return "Bocsánat, de én nem ismerem ezt a mapot. Lehet hogy nagyon új, nagyon nehéz, rankolatlan vagy nem osu!standard map.";
+		return "Bocsanat, de en nem ismerem ezt a mapot. Lehet hogy nagyon uj, nagyon nehez, rankolatlan vagy nem osu!standard map.";
 	}
 
 	@Override
 	public String internalException(String marker) {
-		return "Pff... Úgy látszik hogy az ember Tillerino kihúzta a kábeleimet."
-				+ " Ha nem veszi észre hamarosan, [https://github.com/Tillerino/Tillerinobot/wiki/Contact fel tudnát keresni]? ("+marker+
+		return "Pff... Ugy látszik hogy az ember Tillerino kihuzta a kabeleimet."
+				+ " Ha nem veszi eszre hamarosan, [https://github.com/Tillerino/Tillerinobot/wiki/Contact fel tudnad keresni]? ("+marker+
 				+ " referencia)";
 	}
 
 	@Override
 	public String externalException(String marker) {
-		return "Mi történik? Idétlen kódokat kapok az osu! szerveréről. Megmondanád, hogy ez mit jelentene? 0011101001010000"
-				+ " Az ember Tillerino aztmondja, hogy semmi baj nincs, próbáld meg újra."
-				+ " Ha nagyon megijedtél valamiért, [https://github.com/Tillerino/Tillerinobot/wiki/Contact mondd el neki]." + marker +
+		return "Mi tortenik? Idetlen kodokat kapok az osu! szervererol. Megmondanad, hogy ez mit jelentene? 0011101001010000"
+				+ " Az ember Tillerino aztmondja, hogy semmi baj nincs, probald meg ujra."
+				+ " Ha nagyon megijedtel valamiert, [https://github.com/Tillerino/Tillerinobot/wiki/Contact mondd el neki]." + marker +
 				+ " referencia)";
 	}
 
 	@Override
 	public String noInformationForModsShort() {
-		return "nincs adat a modokról";
+		return "nincs adat a modokrol";
 	}
 
 	@Override
 	public void welcomeUser(IRCBotUser user, OsuApiUser apiUser, long inactiveTime) {
 		if (inactiveTime < 60 * 1000) {
-			user.message("bíp búp");
+			user.message("bip bup");
 		} else if (inactiveTime < 24 * 60 * 60 * 1000) {
-			user.message("De jó újra látni, " + apiUser.getUserName() + ".");
+			user.message("De jo ujra latni, " + apiUser.getUserName() + ".");
 		} else if (inactiveTime > 7l * 24 * 60 * 60 * 1000) {
 			user.message(apiUser.getUserName() + "...");
-			user.message("...te vagy az? De rég láttalak!");
-			user.message("Jó hogy visszajöttél. Kérsz egy ajánlatot?");
+			user.message("...te vagy az? De reg lattalak!");
+			user.message("Jo hogy visszajottel. Kérsz egy ajanlatot?");
 		} else {
 			String[] messages = {
-					"úgy néz ki, hogy szeretnél egy ajánlatot.",
-					"hogy milyen jó látni téged! :)",
-					"te vagy a kedvenc emberem. (Ne mondd el a többi embernek!)",
-					"milyen kedves megelepetés! ^.^",
-					"Reméltem hogy visszajössz. Az összes többi ember unalmas és buta, de ne mondd el nekik hogy ezt mondtam! :3",
+					"ugy nez ki, hogy szeretnel egy ajanlatot.",
+					"hogy milyen jo latni teged! :)",
+					"te vagy a kedvenc emberem. (Ne mondd el a tobbi embernek!)",
+					"milyen kedves meglepetes! ^.^",
+					"Remeltem hogy visszajossz. Az osszes többi ember unalmas es buta, de ne mondd el nekik hogy ezt mondtam!",
 					"hogy vagy ma?",
 			};
 
@@ -78,32 +78,32 @@ public class Default implements Language {
 	@Override
 	public String unknownCommand(String command) {
 		return "Nem ismerem ezt a parancsot: \"" + command
-				+ "\". Írd be azt hogy !help ha a parancsok listájára vagy kiváncsi!";
+				+ "\". Ird be azt hogy !help ha a parancsok listajara vagy kivancsi!";
 	}
 
 	@Override
 	public String noInformationForMods() {
-		return "Bocsi, nem ismerem ezeket a modokat. Nézz vissza később!";
+		return "Bocsi, nem ismerem ezeket a modokat. Nézz vissza kesobb!";
 	}
 
 	@Override
 	public String malformattedMods(String mods) {
-		return "Ömm... ez a modkombináció furán néz ki. A modokat bárhogy lehet kombinálni ezek közül: DT HR HD HT EZ NC FL SO NF. Kombináld őket szóköz, vagy speciális karakter(pl. vessző) nélkül. Példa: !with HDHR, !with DTEZ";
+		return "Omm... ez a modkombinacio furan nez ki. A modokat barhogy lehet kombinalni ezek kozul: DT HR HD HT EZ NC FL SO NF. Kombinald oket szokoz, vagy specialis karakter(pl. vesszo) nelkul. Pelda: !with HDHR, !with DTEZ";
 	}
 
 	@Override
 	public String noLastSongInfo() {
-		return "Nem emlékszem hogy adtam volna neked infót bármelyik dalról... Először menj rá a dalra, írd be azt hogy /np, majd a parancsot amit most beirtál.";
+		return "Nem emlekszem hogy adtam volna neked infot bármelyik dalról... Először menj ra a dalra, ird be azt hogy /np, majd a parancsot amit most beirtal.";
 	}
 
 	@Override
 	public String tryWithMods() {
-		return "Próbáld ki ezt a mapot modokkal!";
+		return "Probald ki ezt a mapot modokkal!";
 	}
 
 	@Override
 	public String tryWithMods(List<Mods> mods) {
-		return "Próbáld ki ezt a mapot " + Mods.toShortNamesContinuous(mods) + " modokkal!";
+		return "Probald ki ezt a mapot " + Mods.toShortNamesContinuous(mods) + " modokkal!";
 	}
 
 	/**
@@ -118,59 +118,59 @@ public class Default implements Language {
 	 * @return
 	 */
 	public String unresolvableName(String exceptionMarker, String name) {
-		return "A neved összezavar. Kibannoltak? Ha nem, légyszives írj [https://github.com/Tillerino/Tillerinobot/wiki/Contact Tillerino]-nak. ("+ exceptionMarker +
+		return "A neved osszezavar. Kibannoltak? Ha nem, legyszives irj [https://github.com/Tillerino/Tillerinobot/wiki/Contact Tillerino]-nak. ("+ exceptionMarker +
 				+ " referencia)";
 	}
 
 	@Override
 	public String excuseForError() {
-		return "Bocsi, Itt volt ez a gyönyörű kód ami egyesekből és nullásokból állt és elkalandoztam. Mit akartál?";
+		return "Bocsi, de itt volt ez a gyonyoru kod ami egyesekbol és nullasokbol allt es elkalandoztam. Mit akartal?";
 	}
 
 	@Override
 	public String complaint() {
-		return "A fellebezésed készen van. Tillerino meg fogja nézni, amint tudja.";
+		return "A fellebezesed keszen van. Tillerino meg fogja nezni, amint tudja.";
 	}
 
 	@Override
 	public void hug(final IRCBotUser user, OsuApiUser apiUser) {
 		user.message("Te! Gyere ide!");
-		user.action("megöleli " + apiUser.getUserName());
+		user.action("megoleli " + apiUser.getUserName() + "t");
 	}
 
 	@Override
 	public String help() {
-		return "Szia! Én vagyok a robot aki megölte Tillerino-t és átvette a profilja felett a hatalmat. Csak viccelek, de nagyon sokat használom ezt a profilt."
-				+ " [https://twitter.com/Tillerinobot státusz és frissítések]"
+		return "Szia! En vagyok a robot aki megolte Tillerino-t es atvette a profilja felett a hatalmat. Csak viccelek, de nagyon sokat hasznalom ezt a profilt."
+				+ " [https://twitter.com/Tillerinobot statusz és frissitesek]"
 				+ " - [https://github.com/Tillerino/Tillerinobot/wiki parancsok]"
 				+ " - [http://ppaddict.tillerino.org/ ppaddict]"
-				+ " - [https://github.com/Tillerino/Tillerinobot/wiki/Contact üzenj]";
+				+ " - [https://github.com/Tillerino/Tillerinobot/wiki/Contact uzenj]";
 	}
 
 	@Override
 	public String faq() {
-		return "[https://github.com/Tillerino/Tillerinobot/wiki/FAQ GY.I.K (Gyakran Ismételt Kérdések)]";
+		return "[https://github.com/Tillerino/Tillerinobot/wiki/FAQ GY.I.K (Gyakran Ismetelt Kerdesek)]";
 	}
 
 	@Override
 	public String featureRankRestricted(String feature, int minRank, OsuApiUser user) {
-		return "Bocsi, de most a " + feature + " szolgáltatás csak azoknak érhető el akinek legalább ennyi rankja van:" + minRank;
+		return "Bocsi, de most a " + feature + " szolgaltatas csak azoknak erheto el akinek legalabb ez a rankja/ennyi ppje van:" + minRank;
 	}
 
 	@Override
 	public String mixedNomodAndMods() {
-		return "Mit akartál mondani a nomoddal és a modokkal?";
+		return "Mit akartal mondani a nomoddal es a modokkal?";
 	}
 
 	@Override
 	public String outOfRecommendations() {
 		return "Mindent elmondtam amit csak tudtam."
-				+ " Próbálj meg más beállításokkal ajánlásokat keresni, vagy használd a !reset parancsot. Ha nem vagy benne biztos, csekkold le az útmutatót: !help.";
+				+ " Próbalj meg más beallitasokkal ajanlasokat keresni, vagy hasznald a !reset parancsot. Ha nem vagy benne biztos, csekkold le az utmutatot: !help.";
 	}
 
 	@Override
 	public String notRanked() {
-		return "Azthiszem ez a beatmap nem ranked. És hát ki akarna nem ranked beatmapból pp-t szerezni...";
+		return "Azthiszem ez a beatmap nem ranked. Es hat ki akarna nem ranked beatmapbol pp-t szerezni...";
 	}
 
 	@Override
@@ -203,7 +203,7 @@ public class Default implements Language {
 
 	@Override
 	public String invalidAccuracy(String acc) {
-		return "Hamis pontosság: \"" + acc + "\"";
+		return "Hamis pontossag: \"" + acc + "\"";
 	}
 
 	@Override
@@ -233,46 +233,46 @@ public class Default implements Language {
 		 * P.S. you can put a link to your profile into the line like this:
 		 * [https://osu.ppy.sh/u/2070907 Tillerino]
 		 */
-		user.message("[https://osu.ppy.sh/u/BakaHarcos BakaHarcos]([http://github.com/ApeConfirmed ApeConfirmed]) megtanított magyarul beszélni. :)");
+		user.message("[https://osu.ppy.sh/u/BakaHarcos BakaHarcos]([http://github.com/ApeConfirmed ApeConfirmed]) megtanitott magyarul beszelni. :)");
 	}
 
 	@Override
 	public String invalidChoice(String invalid, String choices) {
 		return "Bocsi, de a(z) \"" + invalid
-				+ "\" nem értelmes. Próbáld meg ezeket: " + choices + "!";
+				+ "\" nem ertelmes. Próbáld meg ezeket: " + choices + "!";
 	}
 
 	@Override
 	public String setFormat() {
-		return "A parancs a paraméter megadására a !set. Kukkantsd meg a !help-et ha nem vagy benne biztos";
+		return "A parancs a parameter megadasara a !set. Kukkantsd meg a !help-et ha nem vagy benne biztos";
 	}
 	
 	StringShuffler doSomething = new StringShuffler(rnd);
 	
 	@Override
 	public String apiTimeoutException() {
-		final String message = "Az osu! serverek naagggyyyooooonnnn beeellaaaasssuuulltttaak, szóval most nem tudok semmit se csinálni. :V ";
+		final String message = "Az osu! serverek naagggyyyooooonnnn beeellaaaasssuuulltttaak, szoval most nem tudok semmit se csinalni. ";
 		return message + doSomething.get(
-				"Mikor beszéltél utoljára a nagyiddal?",
-				"Addig takarítsd ki a szobád. Hátha addigra jó lesz.",
-				"Jó lenne egyet sétálni most. Tudod...az udvarra?",
-				"Tudok egy ccssommmóóóóó dolgot amit lehetne csinálni. Miért nem csinálod meg őket most?",
-				"Úgy nézel ki, mind egy zombi. Inkább tégy egy pihenőt.",
-				"De csekkold le ezt az érdekes oldalt a [https://hu.wikipedia.org/wiki/Special:Random wikipédián]!",
-				"Cseekold le hogy valaki [http://www.twitch.tv/directory/game/Osu! streamel] e most. Biztos vagyokv benne, hogy találsz valamit",
-				"Nézd, itt egy másik [http://dagobah.net/flash/Cursor_Invisible.swf játék] amiben bisztos hogy szar vagy!",
-				"Legalább lesz egy kis idő, hogy meg nézd az [https://github.com/Tillerino/Tillerinobot/wiki útmutatómat], hogy abbahagyd a hülye parancsok küldözgetését.",
-				"Ne búsulj, ezek a [https://www.reddit.com/r/osugame hülye mémek] biztos elviszik az időt.",
-				"Amíg unatkozol, próbáld ki a [http://gabrielecirulli.github.io/2048/ 2048]-at!",
-				"Vicces kérdés: Ha a merevlemezed bekrepálna, mennyi saját adat veszne el? (Muhahahahaa...)",
-				"Szóvaall... Kipróbáldat már a [https://www.google.hu/search?q=bring%20sally%20up%20push%20up%20challenge sally up push up kihívást]?",
-				"Elmehetsz valami mást csinálni, vagy nézhetünk egymás szemébe folyamatosan. Halkan."
+				"Mikor beszeltel utoljara a nagyiddal?",
+				"Addig takarítsd ki a szobad. Hatha addigra jo lesz.",
+				"Jo lenne egyet setalni most. Tudod... az utcan.",
+				"Tudok egy ccssommmooo dolgot amit lehetne csinalni. Miért nem csinalod meg oket most?",
+				"Ugy nezel ki, mind egy zombi. Inkabb tegy egy pihenot.",
+				"De csekkold le ezt az erdekes oldalt a [https://hu.wikipedia.org/wiki/Special:Random wikipedian]!",
+				"Csekold le hogy valaki [http://www.twitch.tv/directory/game/Osu! streamel] e most. Biztos vagyok benne, hogy talalsz valamit.",
+				"Nezd, itt egy masik [http://dagobah.net/flash/Cursor_Invisible.swf jatek] amiben biztos hogy szar vagy!",
+				"Legalabb lesz egy kis ido, hogy meg nezd az [https://github.com/Tillerino/Tillerinobot/wiki utmutatomat], hogy abbahagyd a hulye parancsok kuldozgeteset.",
+				"Ne busulj, ezek a [https://www.reddit.com/r/osugame hulye memek] biztos elviszik az idot.",
+				"Amig unatkozol, próbáld ki a [http://gabrielecirulli.github.io/2048/ 2048]-at!",
+				"Vicces kerdes: Ha a merevlemezed bekrepalna, mennyi sajat adat veszne el? (Muhahahahaa...)",
+				"Szovaaalll... Kiprobaltad mar a [https://www.google.hu/search?q=bring%20sally%20up%20push%20up%20challenge sally up push up kihivast]?",
+				"Elmehetsz valami mast csinálni, vagy nezhetunk egymas szemebe folyamatosan. Halkan."
 				);
 	}
 
 	@Override
 	public String noRecentPlays() {
-		return "Nem láttalak játszani mostanában.";
+		return "Nem lattalak jatszani mostanaban.";
 	}
 	
 	@Override
@@ -282,6 +282,6 @@ public class Default implements Language {
 	
 	@Override
 	public String getPatience() {
-		return "Csak egy pillanat... Várom hogy Tillerino megszerelje már azt a netkábelt...";
+		return "Csak egy pillanat... Varom hogy Tillerino megszerelje mar azt a netkabelt...";
 	}
 }
