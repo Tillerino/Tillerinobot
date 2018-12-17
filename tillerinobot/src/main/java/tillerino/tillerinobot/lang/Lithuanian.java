9c652484d5d383cdd55e8191e819b346742b6f9e
Simonas G <SlowLogicBoy@users.noreply.github.com>
2018-12-17 11:04:36 +0200

    Update Lithuanian.java (#163)
    Removed some redundant words.
    Removed/Changed some words, because there are some words we don't used on a daily basis.
    Changed some cringy text.
    Remove english words as per recommendations.

--- tillerinobot/src/main/java/tillerino/tillerinobot/lang/Lithuanian.java
+++ tillerinobot/src/main/java/tillerino/tillerinobot/lang/Lithuanian.java
@@ -19,27 +19,27 @@ public class Lithuanian extends AbstractMutableLanguage {
 	
 	@Override
 	public String unknownBeatmap() {
-		return "Atsiprašau, aš nežinau šito grajaus. Jis yra naujas, labai sunkus, nepatvirtintas arba ne standartiniame Osu žaidimo režime.";
+		return "Atsiprašau, bet tokio beatmapo nežinau. Galbūt jis yra naujas, labai sunkus, nepatvirtintas arba ne osu!standard.";
 	}
 
 	@Override
 	public String internalException(String marker) {
-		return "Blemba... žmogus Tillerino sujaukė mano laidus."
-				+ " Jeigu jis artimiausiu metu to nepastebės, ar gali apie tai [https://github.com/Tillerino/Tillerinobot/wiki/Contact jį informuoti]? (reference "
+		return "Velnias... mano kūrėjai susimovė."
+				+ " Jeigu jie artimiausiu metu to nepastebės ir nepataisys, gal galėtum apie [https://github.com/Tillerino/Tillerinobot/wiki/Contact tai pranešti]? (reference "
 				+ marker + ")";
 	}
 
 	@Override
 	public String externalException(String marker) {
-		return "Kas darosi? Aš gaunu tik nesąmones iš Osu! serverio. Ar galėtum pasakyti, ką tai reiškia? 0011101001010000."
-				+ " Žmogus Tillerino sako, kad nėra dėl ko nerimauti ir siūlo pabandyti dar kartą."
-				+ " Jeigu dėl kažkokios priežasties jaudiniesi, gal gali [https://github.com/Tillerino/Tillerinobot/wiki/Contact jį informuoti]?. (reference "
+		return "Nesuprantu kas darosi? osu! servas kažkokias nesąmones siunčia. Ar galėtum pasakyti, ką tai reiškia? 0011101001010000."
+				+ " mano kūrėjai sako, kad čia nieko rimto ir siūlo mėginti dar kartą."
+				+ " Jei manai, kad čia bus kažkas rimčiau gali [https://github.com/Tillerino/Tillerinobot/wiki/Contact mus informuoti]?. (reference "
 				+ marker + ")";
 	}
 
 	@Override
 	public String noInformationForModsShort() {
-		return "Nėra duomenų apie pateiktus modus.";
+		return "Nėra duomenų su pateiktais modais.";
 	}
 
 	@Override
@@ -47,18 +47,18 @@ public class Lithuanian extends AbstractMutableLanguage {
 		if(inactiveTime < 60 * 1000) {
 			return new Message("beep boop");
 		} else if(inactiveTime < 24 * 60 * 60 * 1000) {
-			return new Message("Sveikas sugrįžęs, " + apiUser.getUserName() + ".");
+			return new Message("Labas, " + apiUser.getUserName() + ".");
 		} else if(inactiveTime > 7l * 24 * 60 * 60 * 1000) {
 			return new Message(apiUser.getUserName() + "...")
-				.then(new Message("...ar čia tu? Šimtas metų!"))
-				.then(new Message("Gera tave vėl matyti. Ar galiu tave sudominti pasiūlymu?"));
+				.then(new Message("...Ar čia tikrai tu?"))
+				.then(new Message("Labas. Gal kažką parekomenduoti?"));
 		} else {
 			String[] messages = {
-					"atrodo, kad tu nori rekomendacijos.",
-					"kaip malonu tave matyti! :)",
-					"mano mėgstamiausias žmogus. (Nesakyk kitiems žmonėms!)",
-					"koks malonus siurprizas! ^.^",
-					"ikėjausi, jog pasirodysi. Kiti žmonės mane užknisa, bet nesakyk, jog aš tai sakiau! :3",
+					"atrodo, kad tau reikia rekomendacijų.",
+					"smagu tave matyti! :)",
+					"mano mėgstamiausias žmogus! (Tik nesakyk to kitiem!)",
+					"ooo nustebinai! ^.^",
+					"laukiau kol pasirodysi. Kiti mirtingieji mane nervina, tik nesakyk to kitiem! :3",
 					"ką ruošiesi šiandien veikti?",
 			};
 			
@@ -71,53 +71,53 @@ public class Lithuanian extends AbstractMutableLanguage {
 	@Override
 	public String unknownCommand(String command) {
 		return "nežinoma komanda \"" + command
-				+ "\". Įvesk !help, jei nori pagalbos!";
+				+ "\". Rašyk !help, jei reikia pagalbos!";
 	}
 
 	@Override
 	public String noInformationForMods() {
-		return "Atleisk, dabar negaliu pasidalinti informacija apie šiuos modus.";
+		return "Atsiprašau, bet neturiu info su šiais modais.";
 	}
 
 	@Override
 	public String malformattedMods(String mods) {
-		return "Turbūt ne taip suvedei šiuos modus. Įvedant modus, gali įrašyti bet kokią kombinaciją šių modų: DT HR HD HT EZ NC FL SO NF. Rašyk juos kartu be jokių tarpų, pvz.: !with DTEZ, !with HDHR.";
+		return "Tikriausiai ne taip suvedei modus. Vedant modus, gali rašyti bet kokią kombinaciją su šiais modais: DT HR HD HT EZ NC FL SO NF. Vesk juos be tarpų, pvz.: !with DTEZ, !with HDHR.";
 	}
 
 	@Override
 	public String noLastSongInfo() {
-		return "Nepamenu, jog tu būtum manęs paklausęs informacijos apie kokią dainą...";
+		return "Aha... O kokiam čia mapui?";
 	}
 
 	@Override
 	public String tryWithMods() {
-		return "Pabandyk šį grajų su kokiais nors modais!";
+		return "Pabandyk šį mapą su kokiais nors modais!";
 	}
 
 	@Override
 	public String tryWithMods(List<Mods> mods) {
-		return "Pabandyk šį grajų su " + Mods.toShortNamesContinuous(mods);
+		return "Pabandyk šitą su " + Mods.toShortNamesContinuous(mods);
 	}
 
 	@Override
 	public String excuseForError() {
-		return "Atleisk, radau grąžų sakinį, sudaryta iš nulių ir vienetų ir aš išsiblaškiau. Pakartok dar kartą, ko norėjai?";
+		return "Atsiprašau, buvau paklydęs tarp nulių ir vienetų. Gali pakartot ko norėjai?";
 	}
 
 	@Override
 	public String complaint() {
-		return "Tavo nusiskundimas buvo pateiktas. Tillerino jį peržiūrės, kai turės laiko.";
+		return "Tavo skundas pateiktas. Tillerino peržiūrės kai prie jo prieis.";
 	}
 
 	@Override
 	public Response hug(OsuApiUser apiUser) {
-		return new Message("Ei, tu, ateik čia!")
+		return new Message("Pss, eikš!")
 			.then(new Action("apkabina " + apiUser.getUserName()));
 	}
 
 	@Override
 	public String help() {
-		return "Sveikas! Aš esu robotas, kuris nužudė Tillerino ir pavogė jo paskyrą. Nepergyvenk, aš tik juokauju, bet aš tikrai dažnai naudojuosi šia paskyra."
+		return "Labas! Aš esu botas, kuris pagrobė Tillerino ir užgrobė jo accountą. Che che, juokauju. Bet šiaip tai rimtai naudojuosi šituo accountu."
 				+ " [https://twitter.com/Tillerinobot būsena ir atnaujinimai]"
 				+ " - [https://github.com/Tillerino/Tillerinobot/wiki komandos]"
 				+ " - [http://ppaddict.tillerino.org/ ppaddict]"
@@ -131,45 +131,45 @@ public class Lithuanian extends AbstractMutableLanguage {
 	
 	@Override
 	public String featureRankRestricted(String feature, int minRank, OsuApiUser user) {
-		return "Atsiprašau, šiuo metu " + feature + " yra prieinama žaidėjams, kurie yra pasiekę " + minRank + " vietą.";
+		return "Atsiprašau, bet " + feature + " yra prieinama tik žaidėjams, kurie yra pasiekę bent " + minRank + " vietą.";
 	}
 	
 	@Override
 	public String mixedNomodAndMods() {
-		return "Ką tu turi omeny, sakydamas nomod su modais?";
+		return "Kaip suprast nomod su modais? Tai su ar be modu?";
 	}
 	
 	@Override
 	public String outOfRecommendations() {
 		return "[https://github.com/Tillerino/Tillerinobot/wiki/FAQ#the-bot-says-its-out-of-recommendations-what-do"
-				+ " Aš pasiūliau, viską, ką galiu]."
-				+ " Pabandyk kitus pasiūlų pasirinkimus arba naudok !reset funkciją. Jei neesi tikras, rašyk !help.";
+				+ " Pasiūliau, viską, ką tau galiu pasiūlyt]."
+				+ " Mėgink kitus modus, nustatymus arba rašyk !reset. Jei nežinai, rašyk !help.";
 	}
 
 	@Override
 	public String notRanked() {
-		return "Panašu, jog šis grajus dar nėra patvirtintas.";
+		return "Šitas mapas dar nėra rankintas.";
 	}
 
 	@Override
 	public String invalidAccuracy(String acc) {
-		return "Negalimas tikslumas: \"" + acc + "\"";
+		return "Neteisingai nurodytas tikslumas: \"" + acc + "\"";
 	}
 
 	@Override
 	public Response optionalCommentOnLanguage(OsuApiUser apiUser) {
-		return new Message("Haganenno išmokė mane lietuvių kalbos :)");
+		return new Message("Haganenno su SlowLogicBoy išmokė mane lietuviškai :)");
 	}
 
 	@Override
 	public String invalidChoice(String invalid, String choices) {
-		return "Atleisk, bet \"" + invalid
+		return "Atsiprašau, bet \"" + invalid
 				+ "\" nesiskaito, pabandyk " + choices + "!";
 	}
 
 	@Override
 	public String setFormat() {
-		return "Komanda, kuri nustato parametrą yra !set nustatymo reikšmė. Pabandyk !help, jeigu tau reikia daugiau patarimų. ";
+		return "Komanda, kuri nustato parametrą yra !set nustatymo reikšmė. Pabandyk !help, jeigu reikia daugiau patarimų. ";
 	}
 	
 	StringShuffler apiTimeoutShuffler = new StringShuffler(rnd);
@@ -177,18 +177,18 @@ public class Lithuanian extends AbstractMutableLanguage {
 	@Override
 	public String apiTimeoutException() {
 		registerModification();
-		final String message = "osu! serveriai labai sulėtėjo ir šiuo momentu niekuo negaliu padėt. ";
+		final String message = "osu! servai lagina ir dabar niekuo negaliu padėt. ";
 		return message + apiTimeoutShuffler.get(
 				"Gal susitvarkom kambarį ir pamėginam vėliau?",
-				"Manau reiktų išeit pasivaikščiot arba pasivažinėt. Žinai... į lauką?",
-				"Esu įsitikinęs, kad turi darbų kuriuos tu turi atlikt. Davai pasidarom bent vieną dabar?",
-				"Žinai, šiuo metu tau reiktų nusnūst.",
-				"Tuo tarpu paskaitom [https://lt.wikipedia.org/wiki/Special:Random šitą] iš Vikipedijos!",
-				"Reiktų pažiūrėt ar kas įdomesnio [http://www.twitch.tv/directory/game/Osu! streamina] osu!",
+				"Manau reiktų pasivaikščiot arba pasivažinėt? Žinai... į lauką?",
+				"Manau, kad turi darbų kuriuos reiktų padaryt. Davai pasidarom bent vieną dabar?",
+				"Nenori dabar nusnūst?",
+				"Tuo tarpu pasiskaitom [https://lt.wikipedia.org/wiki/Special:Random šitą] iš Viki!",
+				"Reiktų pažiūrėt ar kas [http://www.twitch.tv/directory/game/Osu! streamina] osu!",
 				"Pažiūrėk gal kažko nežinai [https://github.com/Tillerino/Tillerinobot/wiki apie mane].",
-				"Be baimės, [https://www.reddit.com/r/osugame čia] rasim kaip pratempt laiką.",
-				"Ė, žie, davai pažiūrim ką kiti [https://osu.ppy.sh/forum/t/117708 mūsiškiai] šneka forume?",
-				"Eik ką nors kitą nuveik, bo kitaip mudu čia sėdėsim tyliai nieko neveikdami."
+				"Be baimės, [https://www.reddit.com/r/osugame čia] rasim kaip prastumt laiką.",
+				"Ė, žie, davai pažiūrim ką kiti [https://osu.ppy.sh/forum/t/117708 lietuviai] šneka forume?",
+				"Eik ką nors kitą nuveik, bo mudu čia sėdėsim nieko neveikdami."
 				);
 	}
 	
@@ -199,6 +199,6 @@ public class Lithuanian extends AbstractMutableLanguage {
 	
 	@Override
 	public String isSetId() {
-		return "Man reikia nuorodos į konkretų sudėtingumą, o ne visą packą.";
+		return "Man reikia linko į konkretų mapą, o ne visą packą.";
 	}
 }
