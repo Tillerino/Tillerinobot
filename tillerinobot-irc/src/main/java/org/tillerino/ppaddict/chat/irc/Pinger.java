e21ff96fdc83ff833886883c07d01fe52c270faf
Tillerino <tillmann.gaida@gmail.com>
2019-05-02 19:32:35 +0200

    Remove some crap from the Pinger

--- tillerinobot-irc/src/main/java/org/tillerino/ppaddict/chat/irc/Pinger.java
+++ tillerinobot-irc/src/main/java/org/tillerino/ppaddict/chat/irc/Pinger.java
@@ -3,7 +3,6 @@ package org.tillerino.ppaddict.chat.irc;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.inject.Inject;
@@ -13,24 +12,23 @@ import org.pircbotx.Utils;
 import org.pircbotx.hooks.events.UnknownEvent;
 import org.slf4j.MDC;
 import org.tillerino.ppaddict.chat.irc.BotRunnerImpl.CloseableBot;
+import org.tillerino.ppaddict.util.Clock;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import tillerino.tillerinobot.IRCBot;
 import tillerino.tillerinobot.rest.BotInfoService.BotInfo;
 
 @Slf4j
 @Singleton
+@RequiredArgsConstructor(onConstructor = @__(@Inject))
 public class Pinger {
 	volatile String pingMessage = null;
 	volatile CountDownLatch pingLatch = null;
-	final AtomicBoolean quit = new AtomicBoolean(false);
 
 	final BotInfo botInfo;
 
-	@Inject
-	public Pinger(BotInfo infoService) {
-		this.botInfo = infoService;
-	}
+	final Clock clock;
 
 	long lastquit = 0l;
 
@@ -40,15 +38,12 @@ public class Pinger {
 		if (pingCalls.incrementAndGet() % 10 != 0) {
 			return;
 		}
-		if(quit.get()) {
-			throw new IOException("ping gate closed");
-		}
 
 		if(!bot.isConnected()) {
-			throw new IOException("bot is no longer connected");			
+			throw new IOException("bot is no longer connected");
 		}
 
-		long time = System.currentTimeMillis();
+		long time = clock.currentTimeMillis();
 
 		synchronized (this) {
 			pingLatch = new CountDownLatch(1);
@@ -63,7 +58,7 @@ public class Pinger {
 			throw new IOException("ping timed out");
 		}
 
-		long ping = System.currentTimeMillis() - time;
+		long ping = clock.currentTimeMillis() - time;
 		MDC.put("ping", ping + "");
 	}
 
