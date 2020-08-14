0c61909650554c943fd63d164eeb31274ae4b0de
Tillmann <Tillmann@host.docker.internal>
2020-08-14 10:28:13 +0200

    Bump things (notably Kohii)

--- tillerinobot/src/test/java/tillerino/tillerinobot/diff/KoohiiTest.java
+++ tillerinobot/src/test/java/tillerino/tillerinobot/diff/KoohiiTest.java
@@ -19,8 +19,8 @@ public class KoohiiTest {
 		try (InputStream is = getClass().getResourceAsStream(
 				"/Fujijo Seitokai Shikkou-bu - Best FriendS -TV Size- (Flask) [Fycho's Insane].osu")) {
 			DifficultyProperties diff = new Koohii().calculate(is, Collections.emptyList());
-			assertEquals(2.0308, diff.getSpeed(), 1E-4);
-			assertEquals(2.5894, diff.getAim(), 1E-4);
+			assertEquals(2.02832, diff.getSpeed(), 3E-3);
+			assertEquals(2.62268, diff.getAim(), 3E-3);
 			assertEquals(313, diff.getAllObjectsCount());
 			assertEquals(229, diff.getCircleCount());
 			assertEquals(404, diff.getMaxCombo());
@@ -32,8 +32,8 @@ public class KoohiiTest {
 		try (InputStream is = getClass().getResourceAsStream(
 				"/xi - FREEDOM DiVE (Nakagawa-Kanon) [FOUR DIMENSIONS].osu")) {
 			DifficultyProperties diff = new Koohii().calculate(is, Collections.emptyList());
-			softly.assertThat(diff.getSpeed()).as("speed").isEqualTo(3.613207, offset(1E-4));
-			softly.assertThat(diff.getAim()).as("aim").isEqualTo(3.297445, offset(1E-4));
+			softly.assertThat(diff.getSpeed()).as("speed").isEqualTo(3.73481, offset(2E-4));
+			softly.assertThat(diff.getAim()).as("aim").isEqualTo(3.38204, offset(1E-3));
 			softly.assertThat(diff.getAllObjectsCount()).as("object count").isEqualTo(1983);
 			softly.assertThat(diff.getCircleCount()).as("circle count").isEqualTo(1646);
 			softly.assertThat(diff.getMaxCombo()).as("max max combo").isEqualTo(2385);
