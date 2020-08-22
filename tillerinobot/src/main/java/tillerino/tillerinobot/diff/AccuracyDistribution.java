20d8cc714c8f36ad7b287c84b9ceab11fe5a43f9
Tillmann <Tillmann@host.docker.internal>
2020-08-22 14:10:08 +0200

    Optimize equations

--- tillerinobot/src/main/java/tillerino/tillerinobot/diff/AccuracyDistribution.java
+++ tillerinobot/src/main/java/tillerino/tillerinobot/diff/AccuracyDistribution.java
@@ -2,6 +2,11 @@ package tillerino.tillerinobot.diff;
 
 import lombok.Value;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.round;
+
 import org.apache.commons.lang3.tuple.Pair;
 
 @Value
@@ -18,38 +23,37 @@ public class AccuracyDistribution {
 		return new AccuracyDistribution(best300s, best100s, allObjects - best300s - best100s - misses, misses);
 	}
 
-	private static Pair<Integer, Double> getBest100s(int allObjects, int x300, int misses, double acc) {
-		int best = Math.min(2 * (allObjects - misses) + x300 * 4, Math.max(allObjects - misses + x300 * 5, (int) Math.round(acc * allObjects * 6)));
-		return Pair.of(best - (allObjects - misses + x300 * 5), (double) best / allObjects / 6);
-	}
-
 	private static Pair<Integer, Integer> getBest300s(int allObjects, int misses, double acc) {
+		acc = acc * allObjects * 6;
+		int intAcc = (int) round(acc);
+		int aomm = allObjects - misses;
 		/*
 		 * Why does this work?
 		 *
-		 * _50s are uniquely determined by _300s and _50s.
+		 * _50s are uniquely determined by _300s and _100s.
 		 * => _50s becomes slack variable.
 		 * => Solution polytope is _300s + _100s <= allObjects - misses.
 		 * (Tip: draw this. It's 2D)
 		 * Figure out analytical, continuous solution.
-		 * => Discrete solution must be within +/- _300s, +/- 5 * _100s bounding box.
+		 * => Discrete solution must be within +/- 1 * _300s, +/- 5 * _100s bounding box.
 		 * Make sure that 0 <= _300s <= allObjects - misses.
 		 *
-		 * Search for _100s can be optimized as well if need be.
+		 * After you've realized that, just mess with the equations until nothing is left
 		 */
-		int guess = (int) Math.round((misses + 6 * allObjects * acc - allObjects) / 5);
-		int best = 0;
+		int guess300s = (int) round((acc - aomm) / 5);
+		int best300s = 0;
 		int best100s = 0;
 		double bestError = Double.POSITIVE_INFINITY;
-		for (int i = Math.max(0, guess - 1); i <= Math.min(allObjects - misses, guess + 1); i++) {
-			Pair<Integer, Double> inner = getBest100s(allObjects, i, misses, acc);
-			double error = Math.abs(acc - inner.getRight());
+		for (int _300s = max(0, guess300s - 1); _300s <= min(aomm, guess300s + 1); _300s++) {
+			int localAcc = min(2 * aomm + _300s * 4, max(aomm + _300s * 5, intAcc));
+			int _100s = localAcc - (aomm + _300s * 5);
+			double error = abs(acc - localAcc);
 			if (error < bestError) {
-				best = i;
+				best300s = _300s;
 				bestError = error;
-				best100s = inner.getLeft();
+				best100s = _100s;
 			}
 		}
-		return Pair.of(best, best100s);
+		return Pair.of(best300s, best100s);
 	}
 }
