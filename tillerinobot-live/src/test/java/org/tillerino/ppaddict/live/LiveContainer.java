f25a56bbca4039dda6709ff1a332bf0733f5f64d
Tillmann Gaida <tillmann.gaida@gmail.com>
2020-12-12 09:27:29 +0100

    Bump a few dependencies (Jackson bump causes problems right now)

--- tillerinobot-live/src/test/java/org/tillerino/ppaddict/live/LiveContainer.java
+++ tillerinobot-live/src/test/java/org/tillerino/ppaddict/live/LiveContainer.java
@@ -20,8 +20,8 @@ public class LiveContainer {
 			.withExposedPorts(8080)
 			.waitingFor(Wait.forHttp("/ready").forStatusCode(200))
 			.withCreateContainerCmdModifier((Consumer<CreateContainerCmd>) cmd ->
-					cmd.withMemory(105 * 1024 * 1024L)
-							.withMemorySwap(105 * 1024 * 1024L))
+					cmd.withMemory(106 * 1024 * 1024L)
+							.withMemorySwap(106 * 1024 * 1024L))
 //			.withLogConsumer((Consumer<OutputFrame>) frame -> System.out.println(frame.getUtf8String().trim()))
 			;
 
