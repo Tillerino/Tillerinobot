1090d240020c1cb9cfd918c9780e9eaf84d6b547
Tillmann Gaida <tillmann.gaida@gmail.com>
2021-06-12 10:02:21 +0200

    adjustments in pp test

--- tillerinobot/src/test/java/tillerino/tillerinobot/diff/OsuScoreTest.java
+++ tillerinobot/src/test/java/tillerino/tillerinobot/diff/OsuScoreTest.java
@@ -1,7 +1,9 @@
 package tillerino.tillerinobot.diff;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
+import org.assertj.core.data.Offset;
 import org.junit.Test;
 import org.tillerino.osuApiModel.Mods;
 import org.tillerino.osuApiModel.OsuApiBeatmap;
@@ -13,6 +15,7 @@ public class OsuScoreTest {
 	@Value
 	static class CBeatmapImpl implements Beatmap {
 		private int amountHitCircles;
+		private int amountSpinners;
 		private double od;
 		private double ar;
 		private double aim;
@@ -41,10 +44,17 @@ public class OsuScoreTest {
 		public int NumHitCircles() {
 			return amountHitCircles;
 		}
+
+		@Override
+		public int NumSpinners() {
+			return amountSpinners;
+		}
 	}
 
 	@Test
 	public void testFreedomDiveCptnXn() throws Exception {
+		// https://osu.ppy.sh/api/get_beatmaps?b=129891
+		// https://osu.ppy.sh/scores/osu/1740197996
 		long mods = 0l;
 		OsuApiScore score = new OsuApiScore();
 		score.setMaxCombo(2384);
@@ -58,17 +68,20 @@ public class OsuScoreTest {
 		apiBeatmap.setOverallDifficulty(8);
 		apiBeatmap.setApproachRate(9);
 		// aim and speed from API
-		CBeatmapImpl beatmap = new CBeatmapImpl(1646,
+		CBeatmapImpl beatmap = new CBeatmapImpl(1646, 2,
 				apiBeatmap.getOverallDifficulty(mods),
 				apiBeatmap.getApproachRate(mods), 3.3820393085479736, 3.7348108291625977,
 				apiBeatmap.getMaxCombo());
 		OsuScore standardScore = new OsuScore(score);
-		assertEquals(566.696, standardScore.getPP(beatmap), 2E-3);
+		double pp = standardScore.getPP(beatmap);
+		// from lazer
+		assertThat(pp).isEqualTo(566, Offset.offset(0.5));
 	}
 
 	@Test
 	public void testFreedomDiveCookiezi() throws Exception {
-		long mods = 24l;
+		// https://osu.ppy.sh/scores/osu/2177560145
+		long mods = Mods.getMask(Mods.Hidden, Mods.HardRock);
 		OsuApiScore score = new OsuApiScore();
 		score.setMaxCombo(2385);
 		score.setCount300(1978);
@@ -81,16 +94,20 @@ public class OsuScoreTest {
 		apiBeatmap.setOverallDifficulty(8);
 		apiBeatmap.setApproachRate(9);
 		// aim and speed from API
-		CBeatmapImpl beatmap = new CBeatmapImpl(1646,
+		CBeatmapImpl beatmap = new CBeatmapImpl(1646, 2,
 				apiBeatmap.getOverallDifficulty(mods),
 				apiBeatmap.getApproachRate(mods), 3.6932241916656494, 3.9626119136810303,
 				apiBeatmap.getMaxCombo());
 		OsuScore standardScore = new OsuScore(score);
-		assertEquals(894.388, standardScore.getPP(beatmap), 2E-3);
+		double pp = standardScore.getPP(beatmap);
+		// from lazer
+		assertThat(pp).isEqualTo(908, Offset.offset(0.5));
 	}
 
 	@Test
 	public void testBestFriendsRafis() throws Exception {
+		// https://osu.ppy.sh/api/get_beatmaps?b=574471
+		// https://osu.ppy.sh/scores/osu/2111593239
 		long mods = Mods.getMask(Mods.Hidden, Mods.HardRock, Mods.DoubleTime);
 		OsuApiScore score = new OsuApiScore();
 		score.setMaxCombo(404);
@@ -104,11 +121,11 @@ public class OsuScoreTest {
 		apiBeatmap.setOverallDifficulty(7.0);
 		apiBeatmap.setApproachRate(9);
 		// aim and speed from API
-		CBeatmapImpl beatmap = new CBeatmapImpl(229,
+		CBeatmapImpl beatmap = new CBeatmapImpl(229, 3,
 				apiBeatmap.getOverallDifficulty(mods),
 				apiBeatmap.getApproachRate(mods), 3.9397411346435547,
 				2.8928098678588867, apiBeatmap.getMaxCombo());
 		OsuScore standardScore = new OsuScore(score);
-		assertEquals(595.042, standardScore.getPP(beatmap), 2); // we're off pretty far here, but it's better than before
+		assertEquals(556.377, standardScore.getPP(beatmap), 2);
 	}
 }
