cd99de0ae3076a0d64cce6766ca7cd86a08c667c
Tillmann Gaida <tillmann.gaida@gmail.com>
2022-01-04 11:58:43 +0100

    Add test implementation of SanDoku that reads from the classpath

--- tillerinobot/src/main/java/tillerino/tillerinobot/diff/sandoku/SanDoku.java
+++ tillerinobot/src/main/java/tillerino/tillerinobot/diff/sandoku/SanDoku.java
@@ -1,6 +1,8 @@
 package tillerino.tillerinobot.diff.sandoku;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URL;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -9,10 +11,14 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.glassfish.jersey.client.proxy.WebResourceFactory;
 import org.tillerino.osuApiModel.types.BitwiseMods;
 import org.tillerino.osuApiModel.types.GameMode;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Client for an instance of
@@ -32,5 +38,21 @@ public interface SanDoku {
 
 	static SanDoku defaultClient(URI baseUri) {
 		return WebResourceFactory.newResource(SanDoku.class, JerseyClientBuilder.createClient().target(baseUri));
+	}
+
+	/**
+	 * Implementation of the {@link SanDoku} interface which reads results from the classpath resources.
+	 */
+	class SanDokuTestImpl implements SanDoku {
+		@Override
+		public SanDokuResponse getDiff(int gameMode, long mods, byte[] beatmap) {
+			String md5 = DigestUtils.md5Hex(beatmap);
+			try {
+				URL url = IOUtils.resourceToURL(String.format("/SanDoku/%s-%s-%s.json", md5, gameMode, mods));
+				return new ObjectMapper().readValue(url, SanDokuResponse.class);
+			} catch (IOException e) {
+				throw new RuntimeException();
+			}
+		}
 	}
 }
