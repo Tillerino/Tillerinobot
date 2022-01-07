1f20e2d018ce0a0c89337a3a48f2169e3645a27c
Tillmann Gaida <tillmann.gaida@gmail.com>
2022-01-07 17:47:41 +0100

    deal with spotbugs

--- tillerinobot/src/main/java/tillerino/tillerinobot/diff/sandoku/SanDokuError.java
+++ tillerinobot/src/main/java/tillerino/tillerinobot/diff/sandoku/SanDokuError.java
@@ -5,6 +5,10 @@ import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+@SuppressFBWarnings(value = { "EI_EXPOSE_REP", "EI_EXPOSE_REP2" },
+		justification = "Yes, but also this is wayyy too annoying to do correctly. It's a DTO, relax.")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SanDokuError(
 		String title,
