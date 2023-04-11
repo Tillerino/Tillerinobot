5956c1daa25f64795a3034c0ab29a18428665ea8
Tillerino <tillmann.gaida@gmail.com>
2023-04-11 20:19:17 +0200

    irc: simple logging

--- tillerinobot-irc/src/main/java/org/tillerino/ppaddict/chat/irc/Main.java
+++ tillerinobot-irc/src/main/java/org/tillerino/ppaddict/chat/irc/Main.java
@@ -2,9 +2,12 @@ package org.tillerino.ppaddict.chat.irc;
 
 import io.undertow.Undertow;
 import io.undertow.util.Headers;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Main {
 	public static void main(String[] args) {
+		log.info("starting irc");
 		Undertow server = Undertow.builder()
 			.addHttpListener(8080, "0.0.0.0")
 			.setHandler(exchange -> {
