e18e74bb771bc94f3622a0fa54db4b0b9b2d9ef4
Tillerino <tillmann.gaida@gmail.com>
2023-04-12 18:32:39 +0200

    put Ngircd container into network and log

--- tillerinobot-irc/src/test/java/org/tillerino/ppaddict/chat/irc/NgircdContainer.java
+++ tillerinobot-irc/src/test/java/org/tillerino/ppaddict/chat/irc/NgircdContainer.java
@@ -1,14 +1,18 @@
 package org.tillerino.ppaddict.chat.irc;
 
+import static org.tillerino.ppaddict.util.DockerNetwork.NETWORK;
+
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
 public class NgircdContainer {
 	public static final GenericContainer NGIRCD = new GenericContainer<>("linuxserver/ngircd:60428df3-ls19")
+			.withNetwork(NETWORK)
 			.withNetworkAliases("irc")
 			.withClasspathResourceMapping("/irc/ngircd.conf", "/config/ngircd.conf", BindMode.READ_ONLY)
 			.withClasspathResourceMapping("/irc/ngircd.motd", "/etc/ngircd/ngircd.motd", BindMode.READ_ONLY)
-			.withExposedPorts(6667);
+			.withExposedPorts(6667)
+			.withLogConsumer(frame -> System.out.println("NGIRCD: " + frame.getUtf8String().trim()));
 
 	static {
 		NGIRCD.start();
